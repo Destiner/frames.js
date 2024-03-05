a99c408b5e75efacd87449e3f1c457cf839e47c2
Michal Kvasničák <michal.kvasnicak@gmail.com>
2024-03-05 18:55:18 +0100

    feat: basic create-frames cli tool

---
+++ .scripts/prepare-create-frames.js
@@ -0,0 +1,33 @@
+/**
+ * This script is used to prepare create-frames package for publishing.
+ *
+ * It copies the templates directory to the root of the package so that it can be used by the create-frames package.
+ */
+
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import * as fse from "@ndelangen/fs-extra-unified";
+import glob from "fast-glob";
+import { rimraf } from "rimraf";
+import { renameSync } from 'node:fs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+await rimraf(resolve(__dirname, "../packages/create-frames/templates"));
+
+await fse.copy(
+  resolve(__dirname, "../templates"),
+  resolve(__dirname, "../packages/create-frames/templates"),
+  {
+    filter: (src) => !src.includes("node_modules"),
+  }
+);
+
+const dotfiles = await glob(
+  resolve(__dirname, "../packages/create-frames/templates/**/.*")
+);
+
+// rename dotfiles to use underscore instead of dot so we can publish them to npm as is
+for (const doftile of dotfiles) {
+  renameSync(doftile, doftile.replace("/.", "/_"));
+}
---
+++ packages/create-frames/create.js
@@ -0,0 +1,128 @@
+import { intro, log, outro, select, confirm, text } from "@clack/prompts";
+import { getTemplates } from "./utils/getTemplates.js";
+import { dirname, relative, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { spawnSync } from "node:child_process";
+import ignore from "ignore";
+import pc from "picocolors";
+import * as fs from "@ndelangen/fs-extra-unified";
+import { detect as detectPackageManager } from "detect-package-manager";
+import { readFileSync, readdirSync, renameSync } from "node:fs";
+import { packageManagerRunCommand } from "./utils/packageManagerRunCommand.js";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+/**
+ *
+ * @param {import('yargs').ArgumentsCamelCase<{ t?: string, n?: string }>} params
+ */
+export async function create(params) {
+  if (params.$0 !== "create-frames") {
+    throw new Error("Invalid command");
+  }
+
+  intro("Welcome to frames.js");
+
+  // @see https://github.com/wevm/frog/blob/bd4c7740f7497d9b146db4ca63e828af8ed448ca/create-frog/create.ts#L13
+  // if there is no -n argument (name, ask for the name of new project)
+  let projectName =
+    params.n ||
+    (await text({
+      message: "Enter the name of your project",
+      placeholder: "my-frames-app",
+      validate(input) {
+        if (!input) {
+          return "Project name is required";
+        }
+
+        return;
+      },
+    }));
+
+  const destDir = resolve(process.cwd(), projectName);
+
+  const template =
+    params.t ||
+    (await select({
+      message: "Choose a template for the project",
+      options: getTemplates().map((template) => ({
+        name: template,
+        value: template,
+      })),
+      defaultValue: "next",
+      validate(input) {
+        if (!input) {
+          return "Template is required";
+        }
+
+        return;
+      },
+    }));
+
+  const templateDir = resolve(__dirname, "./templates", template);
+  const ignoredPatterns = readFileSync(
+    resolve(templateDir, "_gitignore"),
+    "utf-8"
+  );
+  const ignored = ignore().add(ignoredPatterns);
+
+  fs.copySync(templateDir, destDir, {
+    filter(src) {
+      const path = relative(templateDir, src);
+
+      return !path || !ignored.ignores(path);
+    },
+  });
+
+  for (const file of readdirSync(destDir)) {
+    if (!file.startsWith("_")) continue;
+
+    renameSync(
+      resolve(destDir, file),
+      resolve(destDir, file.replace("_", "."))
+    );
+  }
+
+  const pkgJsonPath = resolve(destDir, "package.json");
+  const pkgJson = fs.readJsonSync(pkgJsonPath, "utf-8");
+  pkgJson.name = projectName;
+  fs.writeJSONSync(pkgJsonPath, pkgJson, { spaces: 2 });
+
+  log.success(`Project successfully scaffolded in ${pc.blue(destDir)}!`);
+
+  const pkgManager = await detectPackageManager();
+
+  const wantsToInstallDependencies = await confirm({
+    message: `Do you want to install the dependencies using ${pkgManager}?`,
+    initialValue: true,
+  });
+
+  if (wantsToInstallDependencies) {
+    const result = spawnSync(pkgManager, ["install"], {
+      cwd: destDir,
+      stdio: "ignore",
+    });
+
+    if (result.status !== 0) {
+      log.error(
+        `Failed to install the dependencies, please install them manually.`
+      );
+      process.exit(1);
+    }
+
+    log.success(`Dependencies installed!`);
+  }
+
+  log.message("Next steps:");
+  log.step(
+    `1. Go to the project directory by running: ${pc.blue(`cd ./${projectName}`)}`
+  );
+  log.step(
+    `2. Start the development server by running: ${pc.blue(await packageManagerRunCommand("dev"))}`
+  );
+  log.step(
+    `3. Open your browser and go to ${pc.blue(`http://localhost:3000`)} to see your app running`
+  );
+
+  outro("Done! Your project has been set up! 🎉");
+}
